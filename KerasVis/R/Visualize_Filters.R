78fde6d9b00a87b1ffa7fdda071338ee4f5bb625
Kyung Seo <jkhseo@gmail.com>
2019-01-12 16:54:27 -0600

    Another update

--- KerasVis/R/Visualize_Filters.R
+++ KerasVis/R/Visualize_Filters.R
@@ -19,7 +19,8 @@ selected_filters <- list(list(2L, 4L), list(2L, 7L))
 layer_names <- list("block1_conv2", "predictions")
 #Looping through the layers
 
-visualize_filter <- function(model, selected_filters, layers){
+visualize_filter <- function(model, selected_filters, layer_names){
+  images = collections$deque()
   i <- 0
   j <- 1
   for(layer_name in layer_names){
@@ -28,6 +29,9 @@ visualize_filter <- function(model, selected_filters, layers){
     layer_idx <- visutils$utils$find_layer_idx(model, layer_names[[j]])
     for(filter_num in selected_filters[[j]]){
       temp_img <- kerasvis$visualize_activation(model, layer_idx, filter_indices=filter_num, tv_weight=0L,  max_iter=1L)
+      filter_name <- paste(layer_names[j], filter_num, sep="_")
+      plt$imshow(temp_img)
+      plt$savefig(filter_name ,bbox_inches='tight')
       img$append(temp_img)
 
 
@@ -48,3 +52,4 @@ visualize_filter <- function(model, selected_filters, layers){
 
   }
 }
+visualize_filter(model, selected_filters, layer_names)
