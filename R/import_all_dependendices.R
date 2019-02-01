92989f8ac355e25c433b09bb9aebd632f0e73952
srvander <srvanderplas@gmail.com>
2019-02-01 14:21:08 -0600

    Fix import_all_dependencies so that it doesn't load any packages itself. Adding keras/reticulate as dependencies ensures they'll be added (http://r-pkgs.had.co.nz/namespace.html)

--- R/import_all_dependendices.R
+++ R/import_all_dependendices.R
@@ -1,15 +1,27 @@
+#' Import keras-vis dependencies
+#'
+#' Creates module objects for kerasvis, matplotlib, os, and collections
 import_all_dependendices <- function(){
-  check_and_install_package("keras")
-  library(keras)
-  check_and_install_package("reticulate")
-  library(reticulate)
-  #Changing reticulate to use python 3
-  try(reticulate::use_python(python = '/anaconda3/bin/python3', required = T))
-  kerasvis <<- import("vis.visualization")
-  visutils <<- import("vis.utils")
-  im <<- import("vis.input_modifiers")
-  plt <<-import("matplotlib.pyplot")
-  collections <<- import("collections")
-  os <<- import("os")
+  kerasvis <<- reticulate::import("vis.visualization")
+  visutils <<- reticulate::import("vis.utils")
+  im <<- reticulate::import("vis.input_modifiers")
+  plt <<- reticulate::import("matplotlib.pyplot")
+  collections <<- reticulate::import("collections")
+  os <<- reticulate::import("os")
+}
 
+
+#' List all python versions
+#'
+#' @param regex regular expression to use on path
+#' @param ... additional arguments to py_discover_config
+#' @return character vector of python paths that are available and match regex
+py_version_options <- function(regex = ".*", ...) {
+
+  callfns <- c("py_discover_config", )
+  dots <- list(...)
+
+  pyconfig_args <- names(formals(py_discover_config))
+  versions <- do.call(py_discover_config, dots[names(dots) %in% pyconfig_args])$python_versions
+  versions(grepl(regex, versions))
 }
