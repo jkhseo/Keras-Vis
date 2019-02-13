589f06af14bdf6af8c9a7c4f0792251856ca5f1d
Kyung Seo <jkhseo@gmail.com>
2019-02-13 16:51:51 -0600

    Changed Rprofile

--- R/import_all_dependendices.R
+++ R/import_all_dependendices.R
@@ -2,6 +2,7 @@
 #'
 #' Creates module objects for kerasvis, matplotlib, os, and collections
 import_all_dependendices <- function(){
+  
   kerasvis <<- reticulate::import("vis.visualization")
   visutils <<- reticulate::import("vis.utils")
   im <<- reticulate::import("vis.input_modifiers")
@@ -9,20 +10,3 @@ import_all_dependendices <- function(){
   collections <<- reticulate::import("collections")
   os <<- reticulate::import("os")
 }
-
-
-#' List all python versions
-#'
-#' @param regex regular expression to use on path
-#' @param ... additional arguments to py_discover_config
-#' @return character vector of python paths that are available and match regex
-py_version_options <- function(regex = ".*", ...) {
-
-  callfns <- c("py_discover_config", )
-  dots <- list(...)
-
-  pyconfig_args <- names(formals(py_discover_config))
-  versions <- do.call(py_discover_config, dots[names(dots) %in% pyconfig_args])$python_versions
-  versions(grepl(regex, versions))
-}
-
